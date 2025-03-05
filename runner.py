b4e9baf062bbe994454ce07ed8d8d43583b727a2
Vladislav <vvsvoroshilov@gmail.com>
2025-03-05 23:57:21 +0300

    changes

--- runner.py
+++ runner.py
@@ -1,12 +1,4 @@
-from app import app, db
-from flask_script import Manager, Shell
-
-manager = Manager(app)
-
-def make_shell_context():
-    return dict(app=app, db=db)
-
-manager.add_command('shell', Shell(make_context=make_shell_context))
+from app import app
 
 if __name__ == '__main__':
-    manager.run()
+    app.run()
